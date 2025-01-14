5507053714b59f1c6f7941aa91ae591b9f77fc7b
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-14 19:06:11 +0000

    container config done

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert("hello") // we are using nodejs, not browser.
+
+console.log(3+
+    3);//❌  
+console.log("Jasmeet"); // code readability should always be high.
+
+let name = "Jasmeet";
+let age = 20;
+let isloggedIn = true;
+
+/*
+Number => 2 to power of 53 (range for numbers)
+string => ("", '')
+boolean => (yes,no True,false)
+null => stand alone value
+undefined => (values not assigned but the variable is)
+symbol => (used mostly in react) uniqueness 
+
+object
+*/
+
+console.log(typeof"Jasmeet");
+console.log(typeof null);
+console.log(typeof undefined);
+

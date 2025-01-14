e19ce954726d927e65c46fc4618575f83df2f7ce
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-14 18:30:12 +0000

    container config done

---
+++ 01_basic/variables.js
@@ -0,0 +1,13 @@
+const accountId =  14335;
+let accountEmail = "jasmeet.salhotra04@gmail.com";
+var accountPassword = "132322";
+accountCity = "Delhi";
+
+
+// accountId = 3
+
+accountEmail = "hchchc@gmil.com"
+accountPassword = "hascker"
+accountCity = "Mumbai"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])

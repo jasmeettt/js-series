5d95e495cb408ca24cad14423b219004b5378cf0
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-14 19:29:58 +0000

    conversionOperations

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33"; 
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+console.log(typeof isLoggedIn);
+
+let boolValue = Boolean(isLoggedIn);
+console.log(boolValue);
+
+// 1 => true; 0 => false
+// "Js" => true; "" => false

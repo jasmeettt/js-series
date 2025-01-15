999ff11d5709f66890afd3bbcbdb697b69c42f9a
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-15 19:56:38 +0000

    comparision

--- 01_basic/04_comparision.js
+++ 01_basic/04_comparision.js
@@ -1,8 +1,10 @@
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(undefined >= 0);
-console.log(undefined >= 0);
-console.log(undefined >= 0);
+// console.log(undefined >= 0);
+// console.log(undefined >= 0);
+// console.log(undefined >= 0);
+
+// this can give very confusing outputs so its good to avoid this type of comparision
 

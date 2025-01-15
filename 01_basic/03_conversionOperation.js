d60875f1bc339fdd85b3dbd9d8b524b2f851d580
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-15 19:41:30 +0000

    conversionOperations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -16,3 +16,34 @@ console.log(boolValue);
 
 // 1 => true; 0 => false
 // "Js" => true; "" => false
+
+
+/* ------------------------------------------Operations--------------------------------------------*/
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2); addition
+// console.log(2-2); subtraction
+// console.log(2*2); multiplication
+// console.log(2**2); exponent
+// console.log(2/2); division
+// console.log(2%2); remainder
+
+let str1 = "Jasmeet";
+let str2 = " Salhotra"
+let str3 = str1+str2;
+console.log(str3);
+
+console.log(+"");
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+// link to study prefix and postfix increment
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+

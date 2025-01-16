7db2dc465769a57bd8a9a2f84da163cd842ed145
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-16 19:06:20 +0000

    strings

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -2,3 +2,7 @@ const myName = "Jasmeet";
 const age = 20;
 
 console.log(`Hello, My name is ${myName}, and I am ${age} years old`);
+
+
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#description
+// stings descriptions are important to master.

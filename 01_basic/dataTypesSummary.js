b0461b7063d5934d652b34e3fdb17abc0c0b3c64
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-16 17:49:20 +0000

    dataTypes Summary

--- 01_basic/dataTypesSummary.js
+++ 01_basic/dataTypesSummary.js
@@ -35,3 +35,27 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//  -------------------------------- Stack and Heap Memory -------------------------------------------
+
+// Stack (primitive) 
+let name1 = "jasmeet";
+let name2 = name1;
+name2 = "JASMEET";
+
+console.log(name1);
+console.log(name2);
+
+// Heap (non-primitive)
+let myName = {
+    myName: "Jasmeet",
+    email: "jasmeetsal@gmail.com",
+    age: 20
+}
+
+let myName2 = myName;
+myName2.email = "jasmeet@google.com"
+
+console.log(myName.email);
+console.log(myName2.email);

56994ded52d8d7009ea2ec47938411e2d0df63b2
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-17 20:29:56 +0000

    Arrays

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -15,12 +15,14 @@ const newArray = myArr.join();
 //slice, splice
 
 
-console.log('A ', myArr);
+// console.log('A ', myArr);
 
 const myNewArray = myArr.slice(1, 3) // slice does not manipulate the Array
-console.log(myNewArray, "slice"); // output => [1, 2]
-console.log('B ', myArr); // output => [0, 1, 2, 3, 4, 5]
+// console.log(myNewArray, "slice"); // output => [1, 2]
+// console.log('B ', myArr); // output => [0, 1, 2, 3, 4, 5]
 
 const myNewArray2 = myArr.splice(1, 3); // splice does manipulate the Array
-console.log(myNewArray2, "splice"); // output => [1, 2, 3]
-console.log('C ', myArr); // output => [0, 4, 5]
+// console.log(myNewArray2, "splice"); // output => [1, 2, 3]
+// console.log('C ', myArr); // output => [0, 4, 5]
+
+

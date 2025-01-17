0ede4e25169df676ae08f7d46dadbba144bb37db
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-17 19:32:27 +0000

    Date and Time

--- 01_basic/07_date_and_time.js
+++ 01_basic/07_date_and_time.js
@@ -1,3 +1,23 @@
 // date
 let myDate = new Date();
-console.log();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.toUTCString());
+
+// let myCreatedDate = new Date(2024, 4, 20, 12);
+let myCreatedDate = new Date("2024-05-20");
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toLocaleString());
+
+
+
+let myTimestamp = Date.now();
+console.log(myTimestamp);

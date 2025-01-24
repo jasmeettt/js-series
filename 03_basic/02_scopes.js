1756824664cc3971fe539b6888546baa511f01af
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-24 14:16:18 +0000

    scopes

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -4,8 +4,46 @@ if (true){
     var c = 3;
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one () { 
+    const username = " hitesh";
+
+    function two(){
+        const website = "youtube";
+        // console.log(username);
+    }
+    two();
+    // console.log(website);
+}
+one();
+
+if (true){
+    const username = "jasmeet";
+    if (username === "jasmeet") {
+        const website = " youtube";
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    // console.log(username);
+}
+
+
+// ++++++++++++++++++ intersting +++++++++++++++++
+
+addOne(5);
+function addOne (num) { // we can use this function before initialization
+    return num + 1; 
+}
+ 
+
+
+addTwo(5);
+const addTwo = function (num){ // we cannot use this function before initialization
+     return num + 2; 
+}
 
 

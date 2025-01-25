888ed43f41ca4a1bf7adec850b0818a899b46f37
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-25 20:12:45 +0000

    For In Loop

---
+++ for_in_loop.js
@@ -0,0 +1,13 @@
+const myObj = {
+    js : "javascript",
+    cpp : "c++",
+    rs : "ruby",
+    py : "pyhton"
+}
+
+for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}

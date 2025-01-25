aca03b8b7e068010fcc11f00a39e58d3b24edc19
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-25 19:36:51 +0000

    while, do while loop

--- 05_Iterations/while_do_while_loop.js
+++ 05_Iterations/while_do_while_loop.js
@@ -1,8 +1,30 @@
 // while, do while loop
 
-let i = 0;
-while(i <= 10){;
-    console.log(i);
-    i++;
-}
+// let i = 0;
+// while(i <= 10){
+//     console.log(i);
+//     i+=2;           // (i = i + 2) is written like this
+// }
 
+// let myArr = ["Batman", "Superman", "Flash"]
+
+// let i = 0;
+// while(i < myArr.length){
+//     console.log(myArr[i])
+//     i = i+1;            // (i++) is written like this
+// }
+
+
+let score = 11;         // if the score had been 11, then the output would come 11, because dowhile firstly does the operation then the while operation works(just like the name)
+do {
+    console.log(score);
+    score ++;
+} while (score <=10);
+
+
+// let myArr = ["Batman", "Superman", "Flash"]
+// let i =0;
+// do {
+//     console.log(myArr[i]);
+//     i++;
+// } while (i < myArr.length);

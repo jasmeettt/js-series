81d06cabf581b65a7b00cf2b4da2abed0c2b42d5
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-01-25 13:36:13 +0000

    flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,2 +1,17 @@
 // Control Flow or Logic Flow
 
+const userLoggedIn = true;
+const debitCard = true;
+const loggeInFromGoogle = false;
+const loggeInFromEmail = true;
+
+
+// if (userLoggedIn && debitCard){
+//     console.log("allow to buy course");
+    
+// }
+
+// if (loggeInFromEmail || loggeInFromGoogle) {
+//     console.log(`user logged IN`)
+// }
+

f3497bf2321f401c0f25daeb2c04df62e93006af
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-02-04 19:28:16 +0000

    Static property

---
+++ 09_classes_and_oop/static.js
@@ -0,0 +1,25 @@
+class user{
+    constructor(username){
+        this.username=username
+    }
+    logMe(){
+        console.log(`Username is ${this.username}`)
+    }
+    static createId(){ // this property helps in block this code from any of its instances or any other function
+        return `${this.username}_123`
+    }
+    
+}
+const jasmeet = new user ("jasmeet")
+// console.log(jasmeet.createId())
+
+class Teacher extends user {
+    constructor(username,email,password){
+        super(username); 
+        this.email = email
+        this.password  = password
+    }
+}
+const peo = new Teacher("people","people@Hm.com","123");
+peo.logMe()
+// console.log(peo.createId()) 

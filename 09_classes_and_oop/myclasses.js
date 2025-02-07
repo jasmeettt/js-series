fdf09b497efe8dcb028603a157c3549f2e2a7e88
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-02-07 18:54:57 +0000

    Throw keyword, this keyword and classes

--- 09_classes_and_oop/myclasses.js
+++ 09_classes_and_oop/myclasses.js
@@ -20,21 +20,54 @@
 
 //now the behind the scenes of how classes would work.
 
-function user(username, password, email){
-        this.username =username;
-        this.email =email;
-        this.password = password;
+// function user(username, password, email){
+//         this.username =username;
+//         this.email =email;
+//         this.password = password;
+// }
+
+// user.prototype.encrpytPassword = ()=>{
+//     return `${this.password}abc`
+// }
+
+// user.prototype.changeUserName = ()=>{
+//     return `${this.username.toUpperCase()}`
+// }
+
+
+// let bi = new user("Bingo", "123", "bi@bingo.com")
+// console.log(bi.encrpytPassword())
+// console.log(bi.changeUserName())
+
+
+function person (name, age){
+    this.name = name;
+    this.age = age
 }
 
-user.prototype.encrpytPassword = ()=>{
-    return `${this.password}abc`
+const jasmeet = new person("jasmeet", 20)
+// console.log(jasmeet);
+
+function tea(type){
+    this.type = type;
+    this.describe = ()=>{
+        return `this is a cup of ${this.type}`
+    }
 }
 
-user.prototype.changeUserName = ()=>{
-    return `${this.username.toUpperCase()}`
+const green = new tea("green tea")
+// console.log(green)
+// console.log(green.describe())
+
+function animal (species){
+    if (!new.target) {                                       
+        throw new Error(`Enter "new" keyword`);               // throw error keyword
+        
+    }
+    this.species = species
 }
 
+let dog = new animal("dog")
+let cat =  animal("cat")
 
-let bi = new user("Bingo", "123", "bi@bingo.com")
-console.log(bi.encrpytPassword())
-console.log(bi.changeUserName())
+

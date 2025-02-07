5ae04cb83b9cae0379739c05abf8bc23b790ea04
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-02-07 18:26:35 +0000

    Prototype

--- 09_classes_and_oop/prototype.js
+++ 09_classes_and_oop/prototype.js
@@ -3,12 +3,12 @@ let mychannel = "chai         ";
 
 // console.log(myName.trim().length) // low iq way of doing (not really sir was explaining prototype)
 Object.prototype.trueLength = function(){
-    console.log(`True length is ${this.trim().length}`)
-    console.log(`${this}`)
+    // console.log(`True length is ${this.trim().length}`)
+    // console.log(`${this}`)
 }
 
-myName.trueLength()
-"hitesh   ".trueLength()
+// myName.trueLength()
+// "hitesh   ".trueLength()
 
 
 
@@ -21,6 +21,25 @@ let heroPower = {
     thor: "hammer",
     spiderman: "sling",
     getSpiderPower : function(){
-        console.log(`spidy power is ${this.spiderman}`)
+        // console.log(`spidy power is ${this.spiderman}`)
     }
 }
+
+
+
+let computer = { cpu : 12}
+let lenovo = {screen : "Hd",
+    __proto__ : computer
+}
+let tomHardware ={}
+
+// console.log("computer", lenovo.__proto__)
+
+let genericCar = {tyres : 4}
+
+let tesla = {
+    driver : "AI"
+}
+
+Object.setPrototypeOf(tesla, genericCar);
+console.log(`tesla`, Object.getPrototypeOf(tesla))

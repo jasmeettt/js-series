438e1373a9ef926e6068a35bf6143b8f6898c425
Jasmeet <95878924+jasmeettt@users.noreply.github.com>
2025-02-08 20:13:25 +0000

    Challenges for dom

---
+++ 06_dom/script.js
@@ -0,0 +1,31 @@
+//challenge 1
+document.getElementById("changeTextButton").
+addEventListener("click",function(){
+document.getElementById('myParagraph').innerHTML = "this is new test"
+});
+
+//challenge 2
+document.getElementById('highlightFirstCity').addEventListener("click", function (){
+    const city = document.getElementById('citiesList');
+    city.firstElementChild.classList.add("highlight")
+})
+
+//challenge 3
+document.getElementById('changeOrder').addEventListener("click",function(){
+    document.getElementById('coffeeType').textContent = "Espresso"
+})
+
+//challenge 4
+document.getElementById('addNewItem').addEventListener("click",function(){
+    const li = document.createElement('li');
+    li.textContent = "Eggs";
+    document.getElementById('shoppingList').appendChild(li)
+})
+
+//challenge 5
+document.getElementById('removeLastTask').addEventListener("click", function(){
+    const taskList = document.getElementById('taskList')
+    taskList.firstElementChild.remove()
+})
+
+
